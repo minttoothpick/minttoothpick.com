1e4e0e063a1873473192186883034fed6b05a512
Jeremy Jones <minttoothpick@pm.me>
2021-08-03 12:28:31 -0500

    Cache Google Sheets JSON

--- src/_data/books.js
+++ src/_data/books.js
@@ -1,13 +1,21 @@
 const fetch = require("node-fetch");
+const Cache = require("@11ty/eleventy-cache-assets");
 
 module.exports = async function() {
-  let url = `https://spreadsheets.google.com/feeds/list/${process.env.GOOGLE_SHEETS_ID}/1/public/values?alt=json`;
-  console.log("Fetching from Google Sheets...");
-  return await fetch(url)
-    .then(res => res.json()) // node-fetch option to transform to json
-    .then(json => {
-      return {
-        items: json.feed.entry
-      };
-    });
+  let json = await Cache(`https://spreadsheets.google.com/feeds/list/${process.env.GOOGLE_SHEETS_ID}/1/public/values?alt=json`, {
+    duration: "1d",
+    type: "json"
+  });
+  return {
+    items: json.feed.entry
+  };
+  // let url = `https://spreadsheets.google.com/feeds/list/${process.env.GOOGLE_SHEETS_ID}/1/public/values?alt=json`;
+  // console.log("Fetching from Google Sheets...");
+  // return await fetch(url)
+  //   .then(res => res.json()) // node-fetch option to transform to json
+  //   .then(json => {
+  //     return {
+  //       items: json.feed.entry
+  //     };
+  //   });
 };
