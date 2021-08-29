c83400bf41d93fabceb1d2ae9f0d6ba2add162d6
Jeremy Jones <minttoothpick@pm.me>
2021-08-29 18:15:43 -0500

    Lotsa styling!

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ const Image = require("@11ty/eleventy-img");
  * https://alexpeterhall.com/blog/2021/04/05/responsive-images-eleventy/
  * Then added a wrapping `figure` element and whatever else.
  */
-async function imgShortcode(figClass="", src, alt, figcaption="", sizes="(min-width: 729px) 680px, calc(100vw - 48px)") {
+async function imgShortcode(figClass="", src, alt, figcaption="", sizes="(min-width: 729px) 680px, calc(100vw - 48px)", style="") {
   var widths = [];
   // sizes is already defaulted in func param;
   if (figClass == "align-full-bleed") {
@@ -16,6 +16,8 @@ async function imgShortcode(figClass="", src, alt, figcaption="", sizes="(min-wi
   } else {
     widths = [600, 800, 1000, 1444, 1600, 1900];
   }
+
+  if (!figClass) figClass = "align-none";
 
   let metadata = await Image(src, {
     // Actual widths generated; `null` passes original through as well
@@ -47,7 +49,14 @@ async function imgShortcode(figClass="", src, alt, figcaption="", sizes="(min-wi
   let figureStrOpen = "<figure";
   // If the `figClass` parameter isn't empty, add it to `figure` code
   if (figClass.length > 0) {
-    figureStrOpen = `${figureStrOpen} class="${figClass}">`;
+    figureStrOpen = `${figureStrOpen} class="${figClass}"`;
+  } else {
+    figureStrOpen = `${figureStrOpen}`;
+  }
+
+  // TODO: This style 'injection' is sloppy.
+  if (style.length > 0) {
+    figureStrOpen = `${figureStrOpen} style="${style}">`;
   } else {
     figureStrOpen = `${figureStrOpen}>`;
   }
