79bd076bb352a05b5f73841cae5c13d12b19c7b8
Jeremy Jones <minttoothpick@protonmail.com>
2022-01-28 11:10:25 -0500

    Add note

--- src/notes/notes.11tydata.js
+++ src/notes/notes.11tydata.js
@@ -1,3 +1,12 @@
+// Copy live UTC ISO datetime: https://greenwichmeantime.com/articles/clocks/iso/
+/* Template:
+---
+title: "2022-01-27"
+date:
+location: Asheville, NC, USA
+tags:
+---
+*/
 module.exports = {
   layout: "article",
   tags: [ "notes" ],
